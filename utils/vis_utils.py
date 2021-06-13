3a0b440a63e77f3336a1aafaf5f1e666e0d68b57
Vlad <vladislaw.sorockin@yandex.ru>
2021-06-14 01:31:28 +0300

    Add labels to vis_utils, batch support

--- utils/vis_utils.py
+++ utils/vis_utils.py
@@ -23,19 +23,23 @@ def extract_sequences(ampl, labels, time):
 def log_image(result, batch, batch_nb):
     if not os.path.exists('preds'):
         os.makedirs('preds', exist_ok=True)
-    plt.subplot(211)
-    subdata = extract_sequences(batch['ampl_unormalized'].detach().cpu().int().numpy()[0, :, 0],
-                                     batch['target'].detach().cpu().int().numpy()[0, :, 0],
-                                     batch['time_unormalized'].detach().cpu().int().numpy()[0, :, 0])
-    for sdata in subdata:
-        color = ['#008B8B', '#DC143C'][sdata['target'][0]]
-        plt.plot(sdata['time'], sdata['ampl'], color=color)
-    plt.subplot(212)
-    subdata = extract_sequences(batch['ampl_unormalized'].detach().cpu().int().numpy()[0, :, 0],
-                                     result.detach().cpu().int().numpy()[0, :, 0],
-                                     batch['time_unormalized'].detach().cpu().int().numpy()[0, :, 0])
-    for sdata in subdata:
-        color = ['#008B8B', '#DC143C'][sdata['target'][0]]
-        plt.plot(sdata['time'], sdata['ampl'], color=color)
-    plt.savefig(f'preds/img_{batch["id"][0].item()}.jpg')
-    plt.close()
+    len_data = len(batch['time'])
+    for i in range(len_data):
+        plt.subplot(211)
+        subdata = extract_sequences(batch['ampl_unormalized'].detach().cpu().int().numpy()[i, :, 0],
+                                         batch['target'].detach().cpu().int().numpy()[i, :, 0],
+                                         batch['time_unormalized'].detach().cpu().int().numpy()[i, :, 0])
+        for sdata in subdata:
+            color = ['#008B8B', '#DC143C'][sdata['target'][0]]
+            plt.plot(sdata['time'], sdata['ampl'], color=color)
+        plt.title('GT')
+        plt.subplot(212)
+        subdata = extract_sequences(batch['ampl_unormalized'].detach().cpu().int().numpy()[i, :, 0],
+                                         result.detach().cpu().int().numpy()[i, :, 0],
+                                         batch['time_unormalized'].detach().cpu().int().numpy()[i, :, 0])
+        for sdata in subdata:
+            color = ['#008B8B', '#DC143C'][sdata['target'][0]]
+            plt.plot(sdata['time'], sdata['ampl'], color=color)
+        plt.title('Pred')
+        plt.savefig(f'preds/img_{batch["id"][i].item()}.jpg')
+        plt.close()
