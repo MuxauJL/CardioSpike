d7ae51273a8cf06afd133a4b338062e964006e57
Vlad <vladislaw.sorockin@yandex.ru>
2021-06-14 03:42:29 +0300

    add clipping loss

--- models/losses.py
+++ models/losses.py
@@ -3,7 +3,13 @@ import torch
 
 
 class WeightedBCEWithLogits(nn.BCEWithLogitsLoss):
-    def __init__(self, weight=None, size_average=None, reduce=None, reduction: str = 'mean', pos_weight=None):
+    def __init__(self, weight=None, clip=None, size_average=None, reduce=None, reduction: str = 'mean', pos_weight=None):
         pos_weight = torch.tensor(pos_weight)
+        self.clip_min = None if clip is None or clip == 0.0 else torch.logit(torch.tensor(clip))
+        self.clip_max = None if clip is None or clip == 0.0 else torch.logit(torch.tensor(1 - clip))
         super().__init__(weight, size_average, reduce, reduction, pos_weight)
 
+    def forward(self, input, target):
+        if self.clip_min is not None:
+            input = torch.clip(input, self.clip_min, self.clip_max)
+        return super().forward(input, target)
