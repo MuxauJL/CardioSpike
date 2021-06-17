ef819bcf8d9ad2f35cb4a1d3215080bb66dd8117
Mikhail Viryasov <viryasov.mikhail@gmail.com>
2021-06-18 01:24:43 +0300

    Added code for reproducing experiment

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import torch.optim as optim
 
 from dataset import CardioSpikeDataset, conv_collate_fn
 from metrics import binary_classification_results, \
-    binary_classification_metrics_by_counts
+    binary_classification_metrics_by_counts, calc_sklearn_f1_score
 from model import CRNN
 
 device = 'cuda:0'
@@ -58,7 +58,7 @@ def train_model(model, train_loader, val_loader, optimizer, num_epochs=1, schedu
     train_loss_history = []
     val_loss_history = []
     val_f1_history = []
-    best_score = 0
+    best_f1 = 0
     for epoch in range(num_epochs):
         model.train()
 
@@ -78,15 +78,15 @@ def train_model(model, train_loader, val_loader, optimizer, num_epochs=1, schedu
         train_loss_history.append(float(train_ave_loss))
 
         # validation
-        val_freq = 10
-        if epoch > 100:
-            val_freq = 1
+        val_freq = 1
         if epoch % val_freq == 0:
             val_loss_accum = 0
             val_total_count = 0
             tp_accum, fp_accum, fn_accum, correct_accum, total_accum = 0, 0, 0, 0, 0
             model.eval()
             with torch.no_grad():
+                gts = np.array([])
+                preds = np.array([])
                 for i_step, (x, y) in enumerate(val_loader):
                     x_device = x.to(device)
                     y_device = y.to(device)
@@ -103,11 +103,19 @@ def train_model(model, train_loader, val_loader, optimizer, num_epochs=1, schedu
                     val_loss_accum += compute_loss(prediction, y_device)
                     val_total_count += 1
 
+                    gts = np.append(gts, y_device.detach().cpu())
+                    preds = np.append(preds, (probs > 0.5).detach().cpu())
+
                 precision, recall, f1, accuracy = binary_classification_metrics_by_counts(tp_accum, fp_accum, fn_accum, correct_accum, total_accum)
                 print(f'Epoch {epoch + 1}: val precision = {precision}')
                 print(f'Epoch {epoch + 1}: val recall = {recall}')
-                print(f'Epoch {epoch + 1}: val f1 = {f1}')
                 print(f'Epoch {epoch + 1}: val accuracy = {accuracy}')
+                print(f'Epoch {epoch + 1}: val f1 = {f1}')
+
+                f1_micro = calc_sklearn_f1_score(gts, preds, average="micro")
+                f1_binary = calc_sklearn_f1_score(gts, preds, average="binary")
+                print(f'Epoch {epoch + 1}: sklearn f1 micro = {f1_micro}')
+                print(f'Epoch {epoch + 1}: sklearn f1 binary = {f1_binary}')
 
                 val_ave_loss = val_loss_accum / val_total_count
                 val_loss_history.append(float(val_ave_loss))
@@ -115,12 +123,12 @@ def train_model(model, train_loader, val_loader, optimizer, num_epochs=1, schedu
                 writer.add_scalar('Loss/val', val_ave_loss, epoch + 1)
                 writer.add_scalar('Val/F1', f1, epoch + 1)
 
-                if f1 > best_score:
-                    best_score = f1
-                    torch.save(model.state_dict(), f'{experiment_path}/{epoch}_{precision}_{recall}_{f1}_{accuracy}.pth')
+                if f1 > best_f1:
+                    best_f1 = f1
+                    torch.save(model.state_dict(), f'{experiment_path}/{epoch + 1}_{precision}_{recall}_{f1}_{accuracy}.pth')
 
-        msg = "Epoch %d: train average loss: %f, val average loss: %f, val f1: %f\n" % (
-            epoch + 1, train_ave_loss, val_ave_loss, f1)
+        msg = "Epoch %d: train average loss: %f, val average loss: %f, val f1: %f, val f1 micro: %f, val f1 bin: %f\n" % (
+            epoch + 1, train_ave_loss, val_ave_loss, f1, f1_micro, f1_binary)
         print(msg[:-1])
         with open(f'{experiment_path}/log.txt', 'a+') as f:
             f.write(msg)
@@ -157,12 +165,15 @@ if __name__ == '__main__':
                                              num_workers=4)
 
     model = CRNN(lstm_hidden_dim=256, lstm_layers_count=1).cuda()
-    optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-3)
-    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.995405417351527)
+    lr = [1e-4, 9e-5]
+    exp_names = ['crnn_10_256_from_scratch', 'crnn_10_256_finetune']
+    num_epochs = [91, 12]
+    for train_step in range(2):
+        optimizer = optim.AdamW(model.parameters(), lr=lr[train_step], weight_decay=1e-3)
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.995405417351527)
 
-    exp_name = 'crnn_10_256_from_scratch'
-    loss_history, train_history, val_history = train_model(model, train_loader, val_loader, optimizer,
-                                                           num_epochs=500, scheduler=scheduler,
-                                                           exp_name=exp_name)
-    best_score = max(val_history)
-    print(f'best F1 score = {best_score}')
+        loss_history, train_history, val_history = train_model(model, train_loader, val_loader, optimizer,
+                                                               num_epochs=num_epochs[train_step], scheduler=scheduler,
+                                                               exp_name=exp_names[train_step])
+        best_score = max(val_history)
+        print(f'best F1 score = {best_score}')
